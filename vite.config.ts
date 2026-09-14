8f9b766f3d202f3ddc7f3593b155110954236a14
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2026-09-14 14:42:55 +0000

    feat: add Tailwind CSS support and remove unused files

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [
+    react(),
+    tailwindcss({
+      optimize: {
+        minify: true,
+      },
+    }),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
+  ],
 });
